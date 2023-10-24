987729475855ee197fc6999b5405a05a0cf1d521
matthew-pham21 <matthewpham721@gmail.com>
2023-10-24 16:55:34 -0700

    Made changes to ArrayMethods
    Changed ReverseInPlace and Reversed to catch bugs for even valued functions as well as to properly implement and utilize newArray

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,29 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
+  @Test
+  public void testReverseInPlace2() {
+    int[] input2 = { 2, 3, 4, 5 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] { 5, 4, 3, 2 }, input2);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = { 5, 4, 3, 2 };
+    assertArrayEquals(new int[] { 2, 3, 4, 5 }, ArrayExamples.reversed(input2));
   }
 }
